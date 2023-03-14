54904a3f5d94b5621b8c8e0ee16509ac30ddfedd
i3Crk <85000915+i3Crk@users.noreply.github.com>
2023-03-14 11:08:05 -0700

    c

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const login = document.querySelector('loginSubmit')
 
+login.addEventListener('click', () => {
+   login.classList.add('disabled');
+   setTimeout(() => {
+      button.classList.remove('disabled');
+   }, 1000);
+});
