21071d6070d2561328da4142f85dabf730d52763
i3Crk <85000915+i3Crk@users.noreply.github.com>
2023-03-15 11:28:06 -0700

    f

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const userInput = document.getElementById("userInput")
-const passInput = document.getElementById("passInput")
-const loginSubmit = document.querySelector(".loginSubmit")
-
-loginSubmit.addEventListener('click', function(){
+document.querySelector('.loginSubmit').addEventListener('click', function(){
     this.classList.remove('animate');
     setTimeout(() => { this.classList.add('animate'); }, 100);
   });
