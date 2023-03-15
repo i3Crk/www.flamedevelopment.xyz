11509204fba5844ebb5e4ce6ebf91e16651c718e
i3Crk <85000915+i3Crk@users.noreply.github.com>
2023-03-15 14:55:10 -0700

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-function animateButton() {
-    let loginSubmit = document.querySelector('.loginSubmit');
-    loginSubmit.style.animation = 'shrink-grow 200ms';
-    setTimeout(() => {
-      loginSubmit.style.animation = '';
-    }, 200);
-  }
-  
